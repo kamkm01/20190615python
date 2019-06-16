b501e22a012eee4518941b3a60cef354aadbb5b8
kamkm01 <kamkm01@naver.com>
2019-06-16 15:53:59 +0900

    20190616 다른 예제 추가

--- python_basic/if_test1.py
+++ python_basic/if_test1.py
@@ -1,6 +1,15 @@
+#주석
+'''
 money = True
 if money:
     print("택시를 타고 가라")
 else:
     print('걸어가라')
 a = 32
+'''
+
+pocket = ['paper','cellphone','money']
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    print('걸어가')
